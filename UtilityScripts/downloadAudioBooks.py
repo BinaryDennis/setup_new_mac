2506ff41eec3c00540b1fc25a63bbe72d9c600d5
Dennis Charmington <dennis@charmington.se>
2021-12-14 20:44:29 +0100

    Update script for downloading file

--- UtilityScripts/downloadAudioBooks.py
+++ UtilityScripts/downloadAudioBooks.py
@@ -1,15 +1,23 @@
 import re
 import requests
 import sys
+from pathlib import Path
 
 if(len(sys.argv) < 2):
     exit("missing filename as argument")
 
-fileName = sys.argv[1]
-print("trying to opened file " + fileName)
+url = sys.argv[1]
+print("trying to fetch url " + url)
 
-htmlFile = open(fileName,"rt")
-print("starting to parse file ...")
+def getHtmlFile(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        print('fetched html file successfully')
+    elif response.status_code == 404:
+        print('html file not found')
+        exit()
+    return response.text
+    
 
 def download(url, file_name): 
     print("fetching " + url + " as " + file_name + " ...")
@@ -17,11 +25,17 @@ def download(url, file_name):
         response = requests.get(url)        
         file.write(response.content)
 
-match = re.findall('data-url="//([^>]+.mp3)">(\d+.mp3)', htmlFile.read())
-for m in match:    
-    download("https://" + m[0], m[1])
 
+htmlBody = getHtmlFile(url)
 
-htmlFile.close()
+match = re.findall('data-url="//([^>]+.mp3)">(\d+.mp3)', htmlBody)
+for m in match:  
+    url = "https://" + m[0]
+    file_name = m[1]  
+    path = Path(file_name)
+    if path.is_file():
+        print("file " + file_name + " already exists - skipping download")
+    else:
+        download(url, file_name)
+
 exit()
-
