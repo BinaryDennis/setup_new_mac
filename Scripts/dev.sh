7b47c21e799a66a22e747addc690ba021cb928bc
Dennis Charmington <dennis@charmington.se>
2022-02-01 22:11:39 +0100

    Update dev.sh

--- Scripts/dev.sh
+++ Scripts/dev.sh
@@ -3,6 +3,15 @@
 # DESCRIPTION
 # Copies all snippets to Xcode snippet folder
 
+echo "Installing Swiff"
+cd ~/Downloads
+git clone git@github.com:agens-no/swiff.git 
+cd swiff
+sudo make
+cd ..
+echo ""
+
+
 # EXECUTION
 
 echo "Copying Xcode code snippets..."
