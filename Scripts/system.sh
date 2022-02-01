4f0279bd1aa2c977e2f853ad5f640b331bf0f66b
Dennis Charmington <dennis@charmington.se>
2022-02-01 21:45:27 +0100

    Update system.sh

--- Scripts/system.sh
+++ Scripts/system.sh
@@ -8,6 +8,8 @@
 if ! command -v brew > /dev/null; then
     echo "[SYSTEM] Install Homebrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/charmington/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 else
     echo "[SYSTEM] Update Homebrew"
     brew update
