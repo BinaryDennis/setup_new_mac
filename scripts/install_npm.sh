121f6724c289022d81b27073c4a1e834a32afb51
Dennis Charmington <dennis@charmington.se>
2022-08-07 17:35:29 +0200

    Update install_npm script to add npm and yarn

--- scripts/install_npm.sh
+++ scripts/install_npm.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 # DESCRIPTION
-# Installs global npm packages.
+# Installs npm and global npm packages.
+
+echo "[NPM] Installing NPM"
+brew install npm
+echo ""
+
+echo "[NPM] Installing Node Version Manager"
+sudo npm install -g nvm
+echo ""
+
+echo "[NPM] Installing Yarn"
+sudo npm install -g yarn
+echo ""
 
 echo "[NPM] Installing Gulp"
 sudo npm install -g gulp-cli
@@ -12,18 +24,13 @@ sudo npm install -g typescript
 echo ""
 
 
-echo "[NPM] Installing Node Version Manager"
-sudo npm install -g nvm
-echo ""
-
-
 echo "[NPM] Installing Firebase tools"
 sudo npm install -g firebase-tools
 echo ""
 
 
 echo "[NPM] Installing Swiff"
-sudo npm install --global swiff
+sudo npm install -g swiff
 echo ""
 
 echo "[NPM] Done"
