10316244abe917da2b3342240db7588bfa1ca47c
Dennis Charmington <dennis@charmington.se>
2026-04-16 15:36:58 +0200

    Update setup_zsh.sh

--- scripts/setup_zsh.sh
+++ scripts/setup_zsh.sh
@@ -8,14 +8,6 @@ echo "[ZSH] Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo ""
 
-echo "[ZSH] Installing powerline fonts"
-git clone https://github.com/powerline/fonts.git --depth=1 && cd fonts && ./install.sh && cd .. && rm -fr fonts
-echo ""
-
-echo "[ZSH] Installing theme powerlevel9k"
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-echo ""
-
 echo "[ZSH] Setting .zshrc"
 mv ~/.zshrc ~/.zshrc.backup 2>/dev/null
 cp resources/configuration-files/.zshrc ~/.
